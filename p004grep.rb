dbfe4f19d989183d9b35b503038916541d3f3d7a
David Duthie <davidduthie019@gmail.com>
2016-06-17 10:19:09 +0800

    get search term and search scope as command line arguments

--- p004grep.rb
+++ p004grep.rb
@@ -1,9 +1,25 @@
 # an attempt to write my own simple grep
+require 'optparse'
 
-file = IO.readlines("notes/Pragmatic Programmer - Ruby - Journal.md")
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: tag-browser.rb [options]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-pPATH", "--path PATH", "Specify input path (file or folder)") do |p|
+    options[:path] = p
+  end
+  opts.on("-sSEARCH", "--search SEARCH", "Specify search term") do |s|
+    options[:search] = s
+  end
+end.parse!
+
+file = IO.readlines(options[:path]) unless options[:path].chr == "/"
 file.each {|line| line.chomp!}
 
-print "What's your search term? "
 search = Regexp.new(gets.chomp)
 matches = file.select {|line| line =~ search}
 puts "Here are your matches:"
