73ced6116110950533f8ded10d5d8d7a9ca3d756
David Duthie <davidduthie019@gmail.com>
2016-08-10 10:40:50 +0800

    add day 1 exercises

---
+++ prolog/favourites.pl
@@ -0,0 +1,29 @@
+% prolog!
+
+% books and authors
+
+wrote('Clive Staples Lewis', 'The Four Loves').
+wrote('Clive Staples Lewis', 'Till We Have Faces').
+wrote('Gilbert Keith Chesterton', 'Orthodoxy').
+wrote('Gilbert Keith Chesterton', 'The Everlasting Man').
+wrote('Neil Postman', 'Amusing Ourselves To Death').
+wrote('Paul', '2 Timothy').
+wrote('Paul', 'Ephesians').
+
+% musicians
+
+plays('Ben Howard', guitar).
+plays('Future Islands', several).
+plays('John Gomm', guitar).
+plays('Kimiko Ishizaka', piano).
+plays('Young Oceans', several).
+
+genre('Ben Howard', folk).
+genre('Future Islands', synthPop).
+genre('John Gomm', unknown).
+genre('Kimiko Ishizaka', classical).
+genre('Young Oceans', contemplativeWorship).
+
+% queries:
+% wrote('Gilbert Keith Chesterton', What).
+% plays(What, guitar).
