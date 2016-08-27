0281376c4b477430f4f193a92c55aca3309cf413
David Duthie <davidduthie019@gmail.com>
2016-08-27 13:29:00 +0800

    fix punctuation

--- erlang/errata.erl
+++ erlang/errata.erl
@@ -2,4 +2,4 @@
 -export([check/1]).
 
 check(success) -> fwrite("success~n");
-check({Error, Message}) -> fwrite("error: ~p~n", [Message])
+check({Error, Message}) -> fwrite("error: ~p~n", [Message]).
