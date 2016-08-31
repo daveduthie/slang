104f997cbbc9339be15334030face787797d7325
David Duthie <davidduthie019@gmail.com>
2016-08-31 18:56:48 +0800

    make message-passing bidirectional
    Instead of just printing the translation,
    the receive loop now throws the translation back.

--- erlang/translate.erl
+++ erlang/translate.erl
@@ -1,19 +1,29 @@
 -module(translate).
--export ([loop/0]).
+-export ([loop/0, translate/2]).
 loop() ->
     receive
-        "casa" ->
-            io:format("house~n"),
+        {Pid, "casa"} ->
+            Pid ! "house",
             loop();
-        "blanca" ->
-            io:format("white~n"),
+        {Pid, "blanca"} ->
+            Pid ! "white",
             loop();
-        _ ->
-            io:format("I don't understand.~n"),
+        {Pid, _} ->
+            Pid ! "I don't understand.",
             loop()
-    end.
+end.
 
 % Notice that the code inside calls loop() three times, without any returns.
 % That’s OK: Erlang is optimized for tail recursion, so there’s very little overhead,
 % as long as the last thing in any receive clause is a loop().
 % We’re basically defining an empty function and looping forever.
+
+translate(To, Word) ->
+    To ! {self(), Word},
+    receive
+        Translation -> Translation
+    end.
+
+% After compiling, run like this:
+% > Translator = spawn(fun translate:loop/0).
+% > translate:translate(Translator, "casa").
